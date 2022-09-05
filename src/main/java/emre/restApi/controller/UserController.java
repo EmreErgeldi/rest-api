b78a046a1a7e593e18207afa499430aba8d2a17b
FuzzyBang <emrergeldi@gmail.com>
2022-09-05 14:04:17 +0300

    get and set data from mongodb

--- src/main/java/emre/restApi/controller/UserController.java
+++ src/main/java/emre/restApi/controller/UserController.java
@@ -2,11 +2,13 @@ package emre.restApi.controller;
 
 import emre.restApi.exception.UserNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import emre.restApi.model.User;
 import emre.restApi.service.UserService;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
@@ -25,33 +27,58 @@ public class UserController {
 
     // Get all Users
     @GetMapping
-    public ResponseEntity<List<User>> getUsers(@RequestParam(required = false) String userName){
+    public ResponseEntity<List<User>> getUsers(@RequestParam(required = false) String userName, HttpServletResponse response){
+        response.addHeader("access-control-allow-methods", "GET, PUSH, PUT, OPTIONS, DELETE");
+        response.addHeader("access-control-allow-headers", "Content-Type");
+        response.addHeader("access-control-allow-origin", "*");
         return new ResponseEntity<>(userService.getUsers(userName), OK);
     }
 
+    /*@RequestMapping(method = RequestMethod.OPTIONS, value="/**")
+    public String manageOptions(HttpServletResponse response)
+    {
+        response.setHeader("Access-Control-Allow-Methods", "GET, PUSH, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        response.setHeader("Access-Control-Expose-Headers", "*");
+        return "Response with header using HttpServletResponse";
+    }*/ // Calismiyo
+
     // Get one user by ID
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable String id){
+    public ResponseEntity<User> getUser(@PathVariable String id, HttpServletResponse response){
+        response.addHeader("access-control-allow-methods", "GET, PUSH, PUT, OPTIONS, DELETE");
+        response.addHeader("access-control-allow-headers", "Content-Type");
+        response.addHeader("access-control-allow-origin", "*");
         return new ResponseEntity<>(getUserById(id), OK);
     }
 
     // Create new User
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User newUser){
+    public ResponseEntity<User> createUser(@RequestBody User newUser, HttpServletResponse response){
+        response.addHeader("access-control-allow-methods", "GET, PUSH, PUT, OPTIONS, DELETE");
+        response.addHeader("access-control-allow-headers", "Content-Type");
+        response.addHeader("access-control-allow-origin", "*");
         return new ResponseEntity<>(userService.createUser(newUser), CREATED);
     }
 
     // Update user by Id
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateUser(@RequestBody User newUser, @PathVariable String id){
+    public ResponseEntity<Void> updateUser(@RequestBody User newUser, @PathVariable String id, HttpServletResponse response){
         userService.updateUser(id, newUser);
+        response.addHeader("access-control-allow-methods", "GET, PUSH, PUT, OPTIONS, DELETE");
+        response.addHeader("access-control-allow-headers", "Content-Type");
+        response.addHeader("access-control-allow-origin", "*");
         return new ResponseEntity<>(OK);
     }
 
     //Delete User by Id
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String id){
+    public ResponseEntity<Void> deleteUser(@PathVariable String id, HttpServletResponse response){
         userService.deleteUSer(id);
+        response.addHeader("access-control-allow-methods", "GET, PUSH, PUT, OPTIONS, DELETE");
+        response.addHeader("access-control-allow-headers", "Content-Type");
+        response.addHeader("access-control-allow-origin", "*");
         return new ResponseEntity<>(OK);
     }
 
--- src/main/java/emre/restApi/model/User.java
+++ src/main/java/emre/restApi/model/User.java
@@ -8,15 +8,24 @@ import org.springframework.data.annotation.Id;
 import java.util.Date;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class User {
     @Id
     private String id;
-
     private String userName;
-
     private String last_Name;
+
+    private double oid;
+    private String meter_oid;
+    private String equipment_id;
+    private String equipment_type;
+    private double ta64;
+    private String meter_ts;
+    private String received_ts;
+    private String meter_status;
+    private String record_oid;
+    private double status;
+    private String last_update;
 
     private Date createDate = new Date();
 }
